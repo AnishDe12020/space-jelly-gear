830a4b5cba44004b5b879a19cf297c613b975c15
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-19 17:24:48 +0530

    feat: dynamically fetch products in home page from geaphcms

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,95 +11,106 @@ import products from "@data/products";
 
 import styles from "@styles/Page.module.scss";
 
-export default function Home({ home }) {
-	const { heroTitle, heroText, heroLink, heroBackground } = home;
-	return (
-		<Layout>
-			<Head>
-				<title>Space Jelly Gear</title>
-				<meta name="description" content="Get your Space Jelly gear!" />
-			</Head>
+export default function Home({ home, products }) {
+  const { heroTitle, heroText, heroLink, heroBackground } = home;
+  return (
+    <Layout>
+      <Head>
+        <title>Space Jelly Gear</title>
+        <meta name="description" content="Get your Space Jelly gear!" />
+      </Head>
 
-			<Container>
-				<h1 className="sr-only">Space Jelly Gear</h1>
+      <Container>
+        <h1 className="sr-only">Space Jelly Gear</h1>
 
-				<div className={styles.hero}>
-					<Link href={heroLink}>
-						<a>
-							<div className={styles.heroContent}>
-								<h2>{heroTitle}</h2>
-								<p>{heroText}</p>
-							</div>
-							<img
-								className={styles.heroImage}
-								src={heroBackground.url}
-								height={heroBackground.height}
-								width={heroBackground.width}
-								alt=""
-							/>
-						</a>
-					</Link>
-				</div>
+        <div className={styles.hero}>
+          <Link href={heroLink}>
+            <a>
+              <div className={styles.heroContent}>
+                <h2>{heroTitle}</h2>
+                <p>{heroText}</p>
+              </div>
+              <img
+                className={styles.heroImage}
+                src={heroBackground.url}
+                height={heroBackground.height}
+                width={heroBackground.width}
+                alt=""
+              />
+            </a>
+          </Link>
+        </div>
 
-				<h2 className={styles.heading}>Featured Gear</h2>
+        <h2 className={styles.heading}>Featured Gear</h2>
 
-				<ul className={styles.products}>
-					{products.slice(0, 4).map((product) => {
-						return (
-							<li key={product.id}>
-								<Link href="#">
-									<a>
-										<div className={styles.productImage}>
-											<img
-												width="500"
-												height="500"
-												src={product.image}
-												alt=""
-											/>
-										</div>
-										<h3 className={styles.productTitle}>{product.name}</h3>
-										<p className={styles.productPrice}>${product.price}</p>
-									</a>
-								</Link>
-								<p>
-									<Button>Add to Cart</Button>
-								</p>
-							</li>
-						);
-					})}
-				</ul>
-			</Container>
-		</Layout>
-	);
+        <ul className={styles.products}>
+          {products.map((product) => {
+            return (
+              <li key={product.slug}>
+                <Link href="#">
+                  <a>
+                    <div className={styles.productImage}>
+                      <img
+                        width={product.image.width}
+                        height={product.image.height}
+                        src={product.image.url}
+                        alt=""
+                      />
+                    </div>
+                    <h3 className={styles.productTitle}>{product.name}</h3>
+                    <p className={styles.productPrice}>${product.price}</p>
+                  </a>
+                </Link>
+                <p>
+                  <Button>Add to Cart</Button>
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </Container>
+    </Layout>
+  );
 }
 
 export const getStaticProps = async () => {
-	const client = new ApolloClient({
-		uri: "https://api-ap-south-1.graphcms.com/v2/cl25yibjg7r8f01xtfr16e1f4/master",
-		cache: new InMemoryCache(),
-	});
+  const client = new ApolloClient({
+    uri: "https://api-ap-south-1.graphcms.com/v2/cl25yibjg7r8f01xtfr16e1f4/master",
+    cache: new InMemoryCache(),
+  });
 
-	const data = await client.query({
-		query: gql`
-			query PageHome {
-				page(where: { slug: "home" }) {
-					id
-					heroLink
-					heroText
-					heroTitle
-					name
-					slug
-					heroBackground {
-						width
-						url
-						height
-					}
-				}
-			}
-		`,
-	});
+  const data = await client.query({
+    query: gql`
+      query PageHome {
+        page(where: { slug: "home" }) {
+          id
+          heroLink
+          heroText
+          heroTitle
+          name
+          slug
+          heroBackground {
+            width
+            url
+            height
+          }
+        }
+        products(first: 4) {
+          name
+          price
+          slug
+          image {
+            height
+            url
+            width
+          }
+        }
+      }
+    `,
+  });
 
-	const home = data.data.page;
+  const home = data.data.page;
+  const products = data.data.products;
 
-	return { props: { home } };
+  return { props: { home, products } };
 };
